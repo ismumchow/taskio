9a0152a7dcdac41cde73004b9aeef6a2e3ef2ead
ismum <ichowdhury4@fordham.edu>
2023-02-14 22:07:54 -0500

    added project cards

--- components/Greetings.tsx
+++ components/Greetings.tsx
@@ -16,14 +16,14 @@ const Greeting = async ( ) => {
     <Card className="w-full py-4 relative">
         <div className="mb-4">
             <h1 className="text-3xl text-gray-700 font-bold mb-4">
-            Hello, {user.firstName}!
+            Hello, {user?.firstName}!
             </h1>
             <h4 className="text-xl text-gray-400">
             Check your daily tasks and schedule
             </h4>
         </div>
         <div>
-            <Button size="large">Today's Schedule</Button>
+            <Button size="large">Todays Schedule</Button>
         </div>
     </Card>
   );
--- components/ProjectCards.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from "react";
-import { Prisma } from "@prisma/client";
-import  Card from './Card'
-import clsx from "clsx";
-
-const projectWithTasks = Prisma.validator<Prisma.ProjectArgs> 
